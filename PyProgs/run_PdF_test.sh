d68507bf14944f1efdec1db31cb476f2b31ab7d3
Alessia Maggi <alessia.maggi@unistra.fr>
2012-08-27 16:32:51 +0200

    Revert "Updated run_script to new test data"
    This reverts commit 1e3150146c8d9cf1a82a79c94e4b5ca7ccef4721.

--- PyProgs/run_PdF_test.sh
+++ PyProgs/run_PdF_test.sh
@@ -5,11 +5,9 @@ data_dir="PdF"
 net_list="YA"
 sta_list="FJS,FLR,FOR,HDL,RVL,SNE,UV01,UV02,UV03,UV04,UV05,UV06,UV07,UV08,UV09,UV10,UV11,UV12,UV13,UV14,UV15"
 comp_list="HHZ"
-out_dir="TEST"
-#start_time="2010-10-14T00:00:00.0Z"
-#end_time="2010-10-14T01:00:00.0Z"
-start_time="2010-10-14T00:14:00.0Z"
-end_time="2010-10-14T00:18:00.0Z"
+out_dir="PdF_test"
+start_time="2010-10-14T00:00:00.0Z"
+end_time="2010-10-14T01:00:00.0Z"
 
 snr_limit=10.0
 n_kurt_min=4
@@ -24,7 +22,7 @@ mkdir -p $WAVELOC_PATH/out/$out_dir/stack
 mkdir -p $WAVELOC_PATH/out/$out_dir/loc
 mkdir -p $WAVELOC_PATH/out/$out_dir/reloc
 
-./SDS_processing.py --datadir=$data_dir --net_list=$net_list --sta_list=$sta_list --comp_list=$comp_list  --starttime=$start_time --endtime=$end_time --c1=4 --c2=10 --kwin=3.0 --resample --fs=50 --kderiv
+./run_SDS_processing_threading.py -n 10 --datadir=$data_dir --net_list=$net_list --sta_list=$sta_list --comp_list=$comp_list  --starttime=$start_time --endtime=$end_time --c1=4 --c2=10 --kwin=2.5 --krec --resample --fs=50 --kderiv
 
 #./run_waveloc_threading.py -t -v -n 1 --time_grid $time_grid --search_grid $search_grid -s $coord_stations -o $out_dir --datadir=$data_dir --dataglob=*kurt_grad.mseed --starttime=$start_time --endtime=$end_time --data_length=600 --data_overlap=20
 #./run_waveloc_threading.py -t -v -n 4 --time_grid $time_grid --search_grid $search_grid -s $coord_stations -o $out_dir --datadir=$data_dir --dataglob=*kurt_grad.mseed --starttime=$start_time --endtime=$end_time --data_length=600 --data_overlap=20 --load_ttimes_buf 
