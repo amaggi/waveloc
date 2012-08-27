2e50b60f54166230d6d470a6209420e16fcbb508
Alessia Maggi <alessia.maggi@unistra.fr>
2012-08-27 16:40:29 +0200

    Revert "Revert "Turned into a function""
    This reverts commit 06867e73422ad3749ebca9eafe77734dacdf2bea.

--- PyProgs/make_SDS_data_links.py
+++ PyProgs/make_SDS_data_links.py
@@ -6,54 +6,65 @@ import numpy as np
 import logging
 from obspy.core import read
 
+def make_SDS_data_links(datadir,dataglob,outdir):
 
-logging.basicConfig(level=logging.DEBUG, format='%(levelname)s : %(asctime)s : %(message)s')
+  data_dir=os.path.abspath(datadir)
+  out_dir=os.path.abspath(outdir)
 
-p=optparse.OptionParser()
-p.add_option('--datadir',action='store',help="data directory")
-p.add_option('--outdir',action='store',help="output directory")
-p.add_option('--dataglob',action='store',help="data glob")
+  all_files=glob.glob(os.path.join(data_dir,dataglob))
 
-(options,arguments)=p.parse_args()
+  filedict={}
+  for filename in all_files:
+    st=read(filename)
+    net=st.traces[0].stats.network
+    sta=st.traces[0].stats.station
+    cha=st.traces[0].stats.channel
+    dirid="%s.%s.%s"%(net,sta,cha)
+    if filedict.has_key(dirid):
+      filedict[dirid].append(filename)
+    else:
+      filedict[dirid]=[filename]
 
-data_dir=os.path.abspath(options.datadir)
-out_dir=os.path.abspath(options.outdir)
-
-all_files=glob.glob(os.path.join(data_dir,options.dataglob))
-
-filedict={}
-for filename in all_files:
-  st=read(filename)
-  print st
-  net=st.traces[0].stats.network
-  sta=st.traces[0].stats.station
-  cha=st.traces[0].stats.channel
-  dirid="%s.%s.%s"%(net,sta,cha)
-  if filedict.has_key(dirid):
-    filedict[dirid].append(filename)
-  else:
-    filedict[dirid]=[filename]
-
-for dirid,filelist in filedict.iteritems():
-  net=dirid.split('.')[0]
-  sta=dirid.split('.')[1]
-  cha=dirid.split('.')[2]
-  dirname=os.path.join(out_dir,net,sta,"%s.D"%cha)
-  try:
-    os.makedirs(dirname)
-    logging.debug("Made directories : %s"%dirname)
-  except OSError:
-    logging.debug("Directories already exist : %s"%dirname)
-    pass
-
-  for my_file in filelist:
-    dest_file=os.path.join(dirname,os.path.basename(my_file))
+  for dirid,filelist in filedict.iteritems():
+    net=dirid.split('.')[0]
+    sta=dirid.split('.')[1]
+    cha=dirid.split('.')[2]
+    dirname=os.path.join(out_dir,net,sta,"%s.D"%cha)
     try:
-      os.symlink(my_file,dest_file)
-      logging.debug("Linked %s"%dest_file)
+      os.makedirs(dirname)
+      logging.info("Made directories : %s"%dirname)
     except OSError:
-      logging.debug("Removing old %s"%dest_file)
-      os.remove(dest_file)
-      os.symlink(my_file,dest_file)
-      logging.debug("Linked %s"%dest_file)
+      logging.debug("Directories already exist : %s"%dirname)
+      pass
+
+    for my_file in filelist:
+      dest_file=os.path.join(dirname,os.path.basename(my_file))
+      try:
+        os.symlink(my_file,dest_file)
+        logging.info("Linked %s"%dest_file)
+      except OSError:
+        logging.debug("Removing old %s"%dest_file)
+        os.remove(dest_file)
+        os.symlink(my_file,dest_file)
+        logging.info("Linked %s"%dest_file)
     
+
+
+if __name__ == '__main__':
+
+  p=optparse.OptionParser()
+
+  p.add_option('--datadir',action='store',help="data directory")
+  p.add_option('--dataglob',action='store',help="data glob")
+  p.add_option('--outdir',action='store',help="output directory")
+  p.add_option('--debug',action='store_true',help="turn on debugging output")
+
+  (options,arguments)=p.parse_args()
+
+  if options.debug:
+    logging.basicConfig(level=logging.DEBUG, format='%(levelname)s : %(asctime)s : %(message)s')
+  else:
+    logging.basicConfig(level=logging.INFO, format='%(levelname)s : %(asctime)s : %(message)s')
+
+  make_SDS_data_links(options.datadir,options.dataglob,options.outdir)
+
