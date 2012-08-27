bbf27b7eb58caa9c842ba44a71d3fd4c9cb2927a
Alessia Maggi <alessia.maggi@unistra.fr>
2012-08-27 16:32:51 +0200

    Revert "Implemented data signature comparison"
    This reverts commit 635d4592d0e67b503d5a32685acc9136ba518e1a.

--- PyProgs/test_processing.py
+++ PyProgs/test_processing.py
@@ -8,69 +8,54 @@ def suite():
   suite.addTest(ProcessingTests('test_processing'))
   return suite
 
-    
-def waveforms_to_signature(base_path,datadir,dataglob,output_filename):
+def setUpModule():
 
+  datadir='TEST'
+  net_list='YA'
+  sta_list="FJS,FLR,FOR,HDL,RVL,SNE,UV01,UV02,UV03,UV04,UV05,UV06,UV07,UV08,UV09,UV10,UV11,UV12,UV13,UV14,UV15"
+  comp_list="HHZ"
+  starttime="2010-10-14T00:14:00.0Z"
+  endtime="2010-10-14T00:18:00.0Z"
+  resample=False
+  fs=None
+  c1=4.0
+  c2=10.0
+  kwin=4
+  krec=False
+  kderiv=True
 
-  sig_file=open(os.path.join(base_path,'data',datadir,output_filename),'w')
-  allfiles=glob.glob(os.path.join(base_path,'data',datadir, dataglob))
+#  do_SDS_processing_setup_and_run(
+#     datadir=datadir,
+#     net_list=net_list,
+#     sta_list=sta_list,
+#     comp_list=comp_list,
+#     starttime=starttime,
+#     endtime=endtime,
+#     resample=resample,
+#     fs=fs,
+#     c1=c1,
+#     c2=c2,
+#     kwin=kwin,
+#     krec=krec,
+#     kderiv=kderiv)
+
+  base_path=os.getenv('WAVELOC_PATH')
+
+  sig_file=open(os.path.join(base_path,'test_data','test_data_signature.dat'),'w')
+  allfiles=glob.glob(os.path.join(base_path,'data',datadir,'*mseed'))
   for filename in allfiles :
     basename=os.path.basename(filename)
     wf=Waveform()
     wf.read_from_file(filename,format='MSEED')
     (maximum, datasum) = wf.compute_signature()
     sig_file.write("%s \t\t %.6f \t %.6f\n"%(basename,maximum,datasum))
- 
+    
+
 
 class ProcessingTests(unittest.TestCase):
 
-  def setUp(self):
-
-    self.base_path=os.getenv('WAVELOC_PATH')
-
-    self.test_datadir='test_data'
-    self.datadir='TEST'
-    self.net_list='YA'
-    self.sta_list="FJS,FLR,FOR,HDL,RVL,SNE,UV01,UV02,UV03,UV04,UV05,UV06,UV07,UV08,UV09,UV10,UV11,UV12,UV13,UV14,UV15"
-    self.comp_list="HHZ"
-    self.starttime="2010-10-14T00:14:00.0Z"
-    self.endtime="2010-10-14T00:18:00.0Z"
-    self.resample=False
-    self.fs=None
-    self.c1=4.0
-    self.c2=10.0
-    self.kwin=4
-    self.krec=False
-    self.kderiv=True
-
-
   def test_processing(self):
-
-    expected_signature_filename = os.path.join(self.base_path,self.test_datadir,'test_data_signature.dat')
-    expected_signature_file = open(expected_signature_filename,'r') 
-    expected_lines=expected_signature_file.readlines()
-
-#    do_SDS_processing_setup_and_run(
-#      datadir=self.datadir,
-#      net_list=self.net_list,
-#      sta_list=self.sta_list,
-#      comp_list=self.comp_list,
-#      starttime=self.starttime,
-#      endtime=self.endtime,
-#      resample=self.resample,
-#      fs=self.fs,
-#      c1=self.c1,
-#      c2=self.c2,
-#      kwin=self.kwin,
-#      krec=self.krec,
-#      kderiv=self.kderiv)
-   
-    waveforms_to_signature(self.base_path,self.datadir,'*mseed','data_signature.dat')
-    signature_filename=os.path.join(self.base_path,'data',self.datadir,'data_signature.dat')
-    signature_file = open(signature_filename,'r') 
-    lines=signature_file.readlines()
-
-    self.assertSequenceEqual(lines,expected_lines)
+    self.assertTrue(True)
 
 if __name__ == '__main__':
 
