670376718467121d1592fbaeed5c75b6b2a4ab94
Alessia Maggi <alessia.maggi@unistra.fr>
2012-08-29 14:57:22 +0200

    Plot all prob density grids

--- PyProgs/plot_locations_static_matplotlib.py
+++ PyProgs/plot_locations_static_matplotlib.py
@@ -14,16 +14,78 @@ def plot_locations_static_matplotlib(grid4D,grid_dict,x_list,base_filename):
   for key in grid_dict.keys():
     files_dict[key] = base_filename + '_' + key + '.pdf'
   
-  print files_dict
-
+  # do 1D plots
+  plt.clf()
   plt.plot(x0,grid_dict['prob_x0'])
   plt.xlabel('x (km)')
   plt.ylabel('p(x)')
   plt.title('Marginal probability density over x')
   plt.savefig(files_dict['prob_x0'])
+
+  plt.clf()
+  plt.plot(x1,grid_dict['prob_x1'])
+  plt.xlabel('y (km)')
+  plt.ylabel('p(y)')
+  plt.title('Marginal probability density over y')
+  plt.savefig(files_dict['prob_x1'])
+
+  plt.clf()
+  plt.plot(x2,grid_dict['prob_x2'])
+  plt.xlabel('z (km)')
+  plt.ylabel('p(z)')
+  plt.title('Marginal probability density over z')
+  plt.savefig(files_dict['prob_x2'])
+
+  plt.clf()
+  plt.plot(x3,grid_dict['prob_x3'])
+  plt.xlabel('time (s)')
+  plt.ylabel('p(t)')
+  plt.title('Marginal probability density over time')
+  plt.savefig(files_dict['prob_x3'])
+
+  # 2D plots
+  plt.clf()
+  plt.contourf(x0,x3,grid_dict['prob_x0_x3'].T)
+  plt.xlabel('x (km)')
+  plt.ylabel('time (s)')
+  plt.title('Marginal probability density over x and time')
+  plt.savefig(files_dict['prob_x0_x3'])
   
+  plt.clf()
+  plt.contourf(x1,x3,grid_dict['prob_x1_x3'].T)
+  plt.xlabel('y (km)')
+  plt.ylabel('time (s)')
+  plt.title('Marginal probability density over y and time')
+  plt.savefig(files_dict['prob_x1_x3'])
 
+  plt.clf()
+  plt.contourf(x2,x3,grid_dict['prob_x2_x3'].T)
+  plt.xlabel('z (km)')
+  plt.ylabel('time (s)')
+  plt.title('Marginal probability density over z and time')
+  plt.savefig(files_dict['prob_x2_x3'])
+ 
+  plt.clf()
+  plt.contourf(x0,x1,grid_dict['prob_x0_x1'].T)
+  plt.xlabel('x (km)')
+  plt.ylabel('y (km)')
+  plt.title('Marginal probability density over x and y')
+  plt.savefig(files_dict['prob_x0_x1'])
+ 
+  plt.clf()
+  plt.contourf(x0,x2,grid_dict['prob_x0_x2'].T)
+  plt.xlabel('x (km)')
+  plt.ylabel('z (km)')
+  plt.title('Marginal probability density over x and z')
+  plt.savefig(files_dict['prob_x0_x2'])
 
+  plt.clf()
+  plt.contourf(x1,x2,grid_dict['prob_x1_x2'].T)
+  plt.xlabel('y (km)')
+  plt.ylabel('z (km)')
+  plt.title('Marginal probability density over y and z')
+  plt.savefig(files_dict['prob_x1_x2'])
+ 
 if __name__ == '__main__':
 
   base_name=os.getenv('WAVELOC_PATH')
