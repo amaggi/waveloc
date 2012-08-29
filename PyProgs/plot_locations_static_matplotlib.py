4e3bfd375c3224ecb0b2b20b8b131e82978ac431
Alessia Maggi <alessia.maggi@unistra.fr>
2012-08-29 14:08:22 +0200

    Simple plotting via matplotlib

---
+++ PyProgs/plot_locations_static_matplotlib.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from visualization import setup_test_grid
+
+def plot_locations_static_matplotlib(grid4D,grid_dict):
+  pass
+
+
+if __name__ == '__main__':
+
+  grid4D, grid_dict = setup_test_grid()
+  plot_locations_static_matplotlib(grid4D,grid_dict)
+  
