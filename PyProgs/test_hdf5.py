dd671f7eab0927826bed4e50ea7721183918bec1
Alessia Maggi <alessia.maggi@unistra.fr>
2012-09-25 00:54:00 +0200

    Added compression test

--- PyProgs/test_hdf5.py
+++ PyProgs/test_hdf5.py
@@ -4,6 +4,7 @@ import numpy as np
 def suite():
   suite = unittest.TestSuite()
   suite.addTest(H5Tests('test_RandomRead'))
+  suite.addTest(H5Tests('test_compression'))
   return suite
 
 class H5Tests(unittest.TestCase):
@@ -30,6 +31,30 @@ class H5Tests(unittest.TestCase):
     # close and remove the hdf5 file
     f.close()
     os.remove(filename)
+
+  def test_compression(self):
+
+    # set up some random data
+    #data=np.random.rand(100,200,50)
+    data=np.ones((100,200,50))
+
+    # put this data in a hdf5 file
+    filename='randomtest.hdf5'
+    f=h5py.File(filename,'w')
+    dset = f.create_dataset('random',data=data)
+    f.close()
+    size_none=os.stat(filename).st_size
+    os.remove(filename)
+
+    # put this data in a hdf5 file
+    f=h5py.File(filename,'w')
+    dset = f.create_dataset('random',data=data,compression='lzf')
+    f.close()
+    size_lzf=os.stat(filename).st_size
+    os.remove(filename)
+
+    # assert statements
+    self.assertLess(size_lzf,size_none)
 
 if __name__ == '__main__':
 
