497194ea68adc1e435b8b61f5599734f69158297
Alessia Maggi <alessia.maggi@unistra.fr>
2012-09-28 17:11:03 +0200

    More cleanup

--- PyProgs/run_extract.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export WAVELOC_PATH_PDF="$HOME/am_test"
-
-data_dir=14_oct
-out_dir=testing
-
-./extract_located_events.py -o $out_dir -d $data_dir --data_glob *.filt.sac --left_pad 10 --right_pad 20
-
