da3e57d92ab1eb2f60a050be0cf48593eefdebb6
Alessia Maggi <alessia.maggi@unistra.fr>
2013-02-20 18:12:37 +0100

    Going back to old-style recursive kurtosis

--- PyProgs/OP_waveforms.py
+++ PyProgs/OP_waveforms.py
@@ -729,12 +729,12 @@ class Waveform(object):
       xs=np.zeros(npts)    
 
       if recursive:
-        # old style kurtosis
-        #C=1-dt/float(win)
-        #xs=rec_kurtosis_old(x,C)
+         old style kurtosis
+        C=1-dt/float(win)
+        xs=rec_kurtosis_old(x,C)
         # Chassande-Mottin style kurtosis
-        C1=dt/float(win)
-        xs=rec_kurtosis(x,C1)
+        #C1=dt/float(win)
+        #xs=rec_kurtosis(x,C1)
         # smooth xs
         try:
           xs_filt=smooth(xs)
